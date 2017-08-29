76ffded877470a61de72ae332e505007d445b8dd
s1kumar <s1kumar@users.noreply.github.com>
2017-08-29 12:07:12 -0500

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -21,49 +21,44 @@ public class Calculator {
 			double number1 = 0;
 			boolean validNumber = false;
 		        do {
-						System.out.println("Enter a valid first number: ");
-						s1 = sc1.next();
-				        try {
-				        		number1 = Double.parseDouble(s1);
-				        		validNumber = true;
-				        		} catch (Exception e) {
-				        			validNumber = false;
-				        		}
-				} while (validNumber == false);
+				System.out.println("Enter a valid first number: ");
+				s1 = sc1.next();
+				try {
+				number1 = Double.parseDouble(s1);
+				validNumber = true;
+				} catch (Exception e) {
+				validNumber = false;
+				  }
+			} while (validNumber == false);
 		        
 		      	if(number1==0){
 					
-					 System.out.println("Please enter a valid first number");
-					 s1=	sc1.next();
+				System.out.println("Please enter a valid first number");
+				s1=	sc1.next();
 					
-					 number1 = Double.parseDouble(s1);
-				}
-		    //user entering the second number 
-			Scanner sc2= new Scanner(System.in);
+				number1 = Double.parseDouble(s1);
+			}
+		//user entering the second number 
+		Scanner sc2= new Scanner(System.in);
 	        double number2 = 0;
 	        String s2 = "";
 	        boolean validNumber1 = false;
 	        do {
-					System.out.println("Enter a valid second number: ");
-
-			        s2 = sc2.next();
-			        
-			        try {
-			        			number2 = Double.parseDouble(s2);
-			        			validNumber1 = true;
-			        } catch (Exception e) {
-			        			validNumber1 = false;
-			        	}
-			} while (validNumber1 == false);
+			System.out.println("Enter a valid second number: ");
+			s2 = sc2.next();
+			try {
+			number2 = Double.parseDouble(s2);
+			validNumber1 = true;
+			} catch (Exception e) {
+			validNumber1 = false;
+		} while (validNumber1 == false);
 	        
 	      	if(number2==0){
 				
-				 System.out.println("Please enter a valid second number:");
-				 s2=	sc2.next();
-				
-				 number2 = Double.parseDouble(s2);
-				
-			}
+			System.out.println("Please enter a valid second number:");
+			s2=	sc2.next();
+			number2 = Double.parseDouble(s2);
+		}
 	      	
 	      	System.out.println(" Please enter: \n + for Addition,  \n - for subtraction, \n * for multiplication, \n / for division, \n % for modulus, \n p for Power, \n r for Result \n (a+b) ^2 for a+b the whole square, \n a2-b2 for a2-b2,\n pi for PI: ");
 	      	Scanner sc3=new Scanner(System.in);
@@ -75,8 +70,8 @@ public class Calculator {
 		      		
 		     if((sc3.equals(operatorType))){
 				
-				 System.out.println("Please enter a valid operatorType:");
-				 operatorType=	sc3.next();
+			System.out.println("Please enter a valid operatorType:");
+			operatorType=	sc3.next();
 				 
 	        }
 	      	
@@ -84,15 +79,14 @@ public class Calculator {
 			System.out.println("Want to find square root of a number? y/n:");
 			String option=sc4.next();
 			double number=0.00;
-				if(option.equalsIgnoreCase("y")){
-						System.out.println("Enter a double");
-						number = sc4.nextDouble();
-						Square_root sq = new Square_root();
-						double sqr =sq.getSqroot(number);
-						} else if(option.equals("n")) {
-						System.out.println("Please proceed");
-						
-				}
+		if(option.equalsIgnoreCase("y")){
+			System.out.println("Enter a double");
+			number = sc4.nextDouble();
+			Square_root sq = new Square_root();
+			double sqr =sq.getSqroot(number);
+			} else if(option.equals("n")) {
+			ystem.out.println("Please proceed");
+		}
 				Scanner sc5 = new Scanner(System.in);
 				System.out.println("Want to find pi value of a number? y/n:");
 				option=sc5.next();
@@ -112,12 +106,9 @@ public class Calculator {
 				Scanner sc6 = new Scanner(System.in);
 				String	q = sc6.next();
 					if("EXIT".equals(q)){
-						System.out.println("Thank you!!");
-				
-						shouldQuit = true;
+					System.out.println("Thank you!!");
+					shouldQuit = true;
 					}
-					
-					
 					else 	{
 					shouldQuit = false;
 					}
