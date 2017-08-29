cbe90e8c84ac609b3cc5dc20582f8f6338de34c7
s1kumar <s1kumar@users.noreply.github.com>
2017-08-29 12:16:14 -0500

    Update APlusBWholeSquared.java

--- APlusBWholeSquared.java
+++ APlusBWholeSquared.java
@@ -4,9 +4,9 @@ public class APlusBWholeSquared extends Math_Operation{
 	*@author s1kumar 
 	*date:8/28/2017
 	*@version 1.0.0
-	This class has getOperator method  for finding the value of (a+b)2
+	This class has getOperator method for finding the value of (a+b)2
   	taking the numbers passed in by the user.
-		@param   number1 and  number2 ;  getOperator method in this class has parameters named number1 and number2 and at runtime  these parameters will take on the values of the arguments that are passed in by the user. 
+		@param   number1 and  number2 ;getOperator method in this class has parameters named number1 and number2 and at runtime  these parameters will take on the values of the arguments that are passed in by the user. 
 	*/
 	
 	@Override
