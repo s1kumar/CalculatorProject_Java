34f241b67af80c0917bcc93f815598e9866dd323
s1kumar <s1kumar@users.noreply.github.com>
2017-08-30 21:42:52 -0500

    Add files via upload

--- Modulus.java
+++ Modulus.java
@@ -1,20 +1,19 @@
-
-public class Modulus extends Math_Operation {
-	/** this is my <code> Modulus class  </code> 
+/** this is my <code> Modulus</code> 
 	*@author s1kumar 
 	*date:8/28/2017
 	*@version 1.0.0
 	This class has  getOperator method for finding  the modulus value of any two numbers passed in by the user.
 	@param  number1 and number2;getOperator method in this class has parameters named number1 and number2 and at runtime these parameters will take on the values of the arguments that are passed in by the user.
 	*/
-	@Override
+public class Modulus extends Math_Operation {
 	
+	@Override
 	public void getOperator(double number1, double number2){
 		try{
 			System.out.println(number1%number2);
 			
-			} catch (Exception e) {
-				System.out.println("the exception is : "  + e);
+		} catch (Exception e) {
+				System.out.println("the exception is :"  + e);
 		}	
 		
 		
