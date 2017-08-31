db9f447fb8ae59cea239a6b94ea7171083e6e2e8
s1kumar <s1kumar@users.noreply.github.com>
2017-08-30 21:46:25 -0500

    Add files via upload

--- Subtract.java
+++ Subtract.java
@@ -1,14 +1,11 @@
-
-public class Subtract extends Math_Operation  {
-	/** this is my <code> Subtract class  </code> 
+/** this is my <code> Subtract</code> 
 	*@author s1kumar 
 	*date:8/28/2017
 	*@version 1.0.0
 	This class has getOperator method for subtracting the user input numbers.
 		@param  number1 and number2 ; getOperator method in this class has parameters named  number1 and number2 and at runtime  these parameters will take on the values of the arguments that are passed in by the user.
 	*/
-	
-	
+public class Subtract extends Math_Operation  {
 	
 	@Override
 	public void getOperator(double number1, double number2) {
@@ -16,7 +13,7 @@ public class Subtract extends Math_Operation  {
 		try{
 			System.out.println(number1-number2);
 			} catch (Exception e) {
-				System.out.println("the exception is : "  + e);
+				System.out.println("the exception is :"  + e);
 			}
 		
 		
