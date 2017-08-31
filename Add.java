7667539356c93b722a8269baaeb83ff997dfc204
s1kumar <s1kumar@users.noreply.github.com>
2017-08-30 21:37:41 -0500

    Add files via upload

--- Add.java
+++ Add.java
@@ -1,21 +1,17 @@
-
-public class Add extends Math_Operation  {
-	@Override
-	
-	
-		/** this is my <code> Add class  </code> 
+/** this is my <code> Add class  </code> 
 		*@author s1kumar 
 		*date:8/28/2017
 		*@version 1.0.0
 		This  Add class has getOperator method for adding  the  numbers passed in by the user.
 		@param  number1 and number2 ; getOperator method in this class has parameters number1 and number2 and at runtime  these parameters will take on the values of the arguments that are passed in by the user. 
 		*/
-		
-		
+public class Add extends Math_Operation  {
+	@Override
+	
 	public void getOperator(double number1, double number2){
 		try{
-			System.out.println(number1+number2);
-			} catch (Exception e) {
+		System.out.println(number1+number2);
+		} catch (Exception e) {
 			System.out.println("the exception is :"  + e);
 		}
 	}
