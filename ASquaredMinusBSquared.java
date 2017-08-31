ca0306c5f4d926579656ee81d254f8d1a45ed816
s1kumar <s1kumar@users.noreply.github.com>
2017-08-30 21:41:21 -0500

    Add files via upload

--- ASquaredMinusBSquared.java
+++ ASquaredMinusBSquared.java
@@ -1,6 +1,4 @@
-
-public class ASquaredMinusBSquared extends Math_Operation {
-	/** this is my <code> ASquaredMinusBSquared  </code> 
+/** this is my <code> ASquaredMinusBSquared </code> 
 	*@author s1kumar 
 	*date:8/28/2017
 	*@version 1.0.0
@@ -8,13 +6,15 @@ public class ASquaredMinusBSquared extends Math_Operation {
 .
 	  	@param  number1 and number2 ; getOperator method in this class has parameters named  number1 and number2 and at runtime  these parameters will take on the values of the arguments that are passed in by the user.
 	*/
+public class ASquaredMinusBSquared extends Math_Operation {
+	
 	@Override
 	public void getOperator(double number1, double number2){
 		
 		try{
 			System.out.println((number1+number2)*(number1-number2));
-			} catch (Exception e) {
-			System.out.println("the exception is :"  + e);
+		} catch (Exception e) {
+				System.out.println("the exception is :"  + e);
 		}
 		
 	}
