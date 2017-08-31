1508971ac2397e687f517caa8fd6d0adba7d9e21
s1kumar <s1kumar@users.noreply.github.com>
2017-08-30 21:45:21 -0500

    Add files via upload

--- Result.java
+++ Result.java
@@ -1,6 +1,4 @@
-
-public class Result extends Math_Operation  {
-	/** this is my <code> Result class  </code> 
+/** this is my <code> Result</code> 
 	*@author s1kumar 
 	*date:8/28/2017
 	*@version 1.0.0
@@ -8,10 +6,9 @@ public class Result extends Math_Operation  {
 	 	@param  number1 and number2 ; getOperator method in this class has parameters named  number1 and number2 and at runtime  these parameters will take on the values of the arguments that are passed in by the user.
 	*/
 	
-	
+public class Result extends Math_Operation  {
 	
 	@Override
-	
 	public void getOperator(double number1, double number2){
 		try{
 		System.out.println(number1==number2);
