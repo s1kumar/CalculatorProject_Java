22e097220a48801008ec29fe2488878cdbef7142
s1kumar <s1kumar@users.noreply.github.com>
2017-08-30 21:41:47 -0500

    Add files via upload

--- Division.java
+++ Division.java
@@ -1,6 +1,4 @@
-
-public class Division extends Math_Operation  {
-	/** this is my <code> Division class  </code> 
+/** this is my <code> Division</code> 
 	*@author s1kumar 
 	*date:8/28/2017
 	*@version 1.0.0
@@ -8,10 +6,9 @@ public class Division extends Math_Operation  {
 		@param number1 and number2 ; getOperator method in this class has parameters number1 and number2 and at runtime  these parameters will take on the values of the arguments that are passed in by the user.
 	
 	*/
+public class Division extends Math_Operation  {
 	
-	
-	@Override
-	
+@Override
 	public void getOperator(double number1,  double number2){
 		try{
 		
