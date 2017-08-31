c9bcfd2d098c763f7fc3af00baf9d84101de04b3
s1kumar <s1kumar@users.noreply.github.com>
2017-08-30 21:45:58 -0500

    Add files via upload

--- Square_root.java
+++ Square_root.java
@@ -1,16 +1,12 @@
-
-public class Square_root {
-	/** 
-	 * this is my <code> Square_root class  </code> 
+/** 
+	 * this is my <code> Square_root</code> 
 	*@author s1kumar 
 	*date:8/28/2017
 	*@version 1.0.0
 	This class has  getOperator method for finding the square root of the number passed in by the user.
 	@param number ;  getOperator method in this class has parameter named number and at runtime this parameter will take on the value of the argument that is passed in by the user.
 	*/
-	
-	
-	
+public class Square_root {
 	public double number;
 
 	public  double getSqroot(double number){
